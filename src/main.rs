66e26562438f1066b1a5ff3e8c1f54d52f5e41f7
Matt Stobo <mwstobo@gmail.com>
2024-01-21 13:09:26 -0500

    Remove push gateway and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use aws_sdk_route53::model;
+use aws_sdk_route53::types;
 use std::net::ToSocketAddrs;
-use std::{env, io, time};
+use std::{env, io};
 
 async fn current() -> Result<String, reqwest::Error> {
     reqwest::Client::new()
@@ -28,52 +28,26 @@ async fn update(
     host_name: String,
     current: String,
 ) -> Result<(), aws_sdk_route53::Error> {
+    let resource_record = types::ResourceRecord::builder().value(current).build()?;
+    let resource_record_set = types::ResourceRecordSet::builder()
+        .name(host_name)
+        .ttl(300)
+        .r#type(types::RrType::A)
+        .resource_records(resource_record)
+        .build()?;
+    let change = types::Change::builder()
+        .action(types::ChangeAction::Upsert)
+        .resource_record_set(resource_record_set)
+        .build()?;
+    let change_batch = types::ChangeBatch::builder().changes(change).build()?;
     client
         .change_resource_record_sets()
         .hosted_zone_id(hosted_zone_id)
-        .change_batch(
-            model::ChangeBatch::builder()
-                .changes(
-                    model::Change::builder()
-                        .action(model::ChangeAction::Upsert)
-                        .resource_record_set(
-                            model::ResourceRecordSet::builder()
-                                .name(host_name)
-                                .ttl(300)
-                                .r#type(model::RrType::A)
-                                .resource_records(
-                                    model::ResourceRecord::builder().value(current).build(),
-                                )
-                                .build(),
-                        )
-                        .build(),
-                )
-                .build(),
-        )
+        .change_batch(change_batch)
         .send()
         .await
         .map(|_| ())
         .map_err(|e| e.into())
-}
-
-async fn push(
-    push_gateway_host: String,
-    job: &str,
-    current_time: u64,
-) -> Result<(), reqwest::Error> {
-    reqwest::Client::new()
-        .post(format!("{}/metrics/job/{}", push_gateway_host, job))
-        .body(format!(
-            "#TYPE last_successful_execution_timestamp_seconds gauge\n\
-             #HELP last_successful_execution_timestamp_seconds \
-                   Timestamp of the last successful execution of a job\n\
-             last_successful_execution_timestamp_seconds {}\n",
-            current_time
-        ))
-        .send()
-        .await?
-        .error_for_status()
-        .map(|_| ())
 }
 
 struct RequiredEnvVar {
@@ -91,7 +65,6 @@ impl RequiredEnvVar {
 async fn main() {
     let host_name = RequiredEnvVar::new("HOST_NAME").value;
     let hosted_zone_id = RequiredEnvVar::new("HOSTED_ZONE_ID").value;
-    let push_gateway_host = RequiredEnvVar::new("PUSH_GATEWAY_HOST").value;
 
     let external_ip = current().await.expect("Unable to get current IP address");
     let host_ip = lookup(&host_name, 80)
@@ -103,17 +76,10 @@ async fn main() {
 
     if host_ip != external_ip {
         println!("Updating DNS record of {} to {}", host_name, external_ip);
-        let config = aws_config::load_from_env().await;
+        let config = aws_config::load_defaults(aws_config::BehaviorVersion::v2023_11_09()).await;
         let client = aws_sdk_route53::Client::new(&config);
         update(client, hosted_zone_id, host_name, external_ip)
             .await
             .expect("Failed to update DNS records");
     }
-    let current_time = time::SystemTime::now()
-        .duration_since(time::UNIX_EPOCH)
-        .expect("Unable to get current system time")
-        .as_secs();
-    push(push_gateway_host, "dyndns_route53", current_time)
-        .await
-        .expect("Failed to push metrics to push gateway")
 }
